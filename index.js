570967d945f9ed6bb17e876013cde4d72016c2a6
Neil Ricci <iccir919@gmail.com>
2021-04-25 15:52:44 -0700

    Add firebase login

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-// read env vars from .env file
+
 require('dotenv').config();
+require('firebase/analytics');
+require('firebase/auth');
 
 const util = require('util');
 const express = require('express');
@@ -8,37 +10,6 @@ const moment = require('moment');
 const plaid = require('plaid');
 
 const APP_PORT = process.env.APP_PORT || 8000;
-const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
-const PLAID_SECRET = process.env.PLAID_SECRET;
-const PLAID_ENV = process.env.PLAID_ENV || 'sandbox';
-
-// PLAID_PRODUCTS is a comma-separated list of products to use when initializing
-// Link. Note that this list must contain 'assets' in order for the app to be
-// able to create and retrieve asset reports.
-const PLAID_PRODUCTS = (process.env.PLAID_PRODUCTS || 'transactions').split(
-    ',',
-);
-
-// PLAID_COUNTRY_CODES is a comma-separated list of countries for which users
-// will be able to select institutions from.
-const PLAID_COUNTRY_CODES = (process.env.PLAID_COUNTRY_CODES || 'US').split(
-    ',',
-);
-
-// Store the access_token in memory - in production, store it in a secure
-// persistent data store
-let ACCESS_TOKEN = null;
-let PUBLIC_TOKEN = null;
-let ITEM_ID = null;
-
-const client = new plaid.Client({
-    clientID: PLAID_CLIENT_ID,
-    secret: PLAID_SECRET,
-    env: plaid.environments[PLAID_ENV],
-    options: {
-        version: '2019-05-29'
-    },
-});
 
 const app = express();
 app.use(express.static('public'));
@@ -51,63 +22,6 @@ app.use(bodyParser.json());
 
 app.get('/', function (request, response, next) {
     response.sendFile('./views/index.html', { root: __dirname })
-});
-
-app.post('/api/info', function (request, response, next) {
-    console.log('Request received to /api/info');
-    const info = {
-        item_id: ITEM_ID,
-        access_token: ACCESS_TOKEN,
-        products: PLAID_PRODUCTS,
-    };
-    console.log(info);
-    response.json(info)
-});
-
-app.post('/api/create_link_token', function (request, response, next) {
-    const configs = {
-        user: {
-            client_user_id: 'unique-user-id'
-        },
-        client_name: 'Beam',
-        products: PLAID_PRODUCTS,
-        country_codes: PLAID_COUNTRY_CODES,
-        language: 'en'
-    };
-
-    client.createLinkToken( configs, function (error, createTokenResponse ) {
-        if(error != null) {
-            prettyPrintResponse(error);
-            return response.json({
-                error: error,
-            });
-        }
-        console.log("Link token created:")
-        prettyPrintResponse(createTokenResponse)
-        response.json(createTokenResponse);
-    });
-});
-
-app.post('/api/set_access_token', function (request, response) {
-    PUBLIC_TOKEN = request.body.public_token;
-    client.exchangePublicToken(PUBLIC_TOKEN, function (error, tokenResponse) {
-        if (error != null) {
-            prettyPrintResponse(error);
-            return response.json({
-                error,
-            })
-        }
-
-        ACCESS_TOKEN = tokenResponse.access_token;
-        ITEM_ID = tokenResponse.item_id;
-        console.log("Access token created:")
-        prettyPrintResponse(tokenResponse);
-        response.json({
-            access_token: ACCESS_TOKEN,
-            item_id: ITEM_ID,
-            error: null,
-        });
-    });
 });
 
 const server = app.listen (APP_PORT, function () {
