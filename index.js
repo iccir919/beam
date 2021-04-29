7ce8b05a28bc1097c25342b76489d0a31e80c474
Neil Ricci <iccir919@gmail.com>
2021-04-29 08:17:59 -0700

    Implemented authentication via Firebase

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 
 require('dotenv').config();
-require('firebase/analytics');
-require('firebase/auth');
-
 const util = require('util');
 const express = require('express');
 const bodyParser = require('body-parser');
 const moment = require('moment');
 const plaid = require('plaid');
+const firebaseAdmin = require('firebase-admin');
+const serviceAccount = require("./serviceAccountKey.json");
 
 const APP_PORT = process.env.APP_PORT || 8000;
+
+firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert(serviceAccount),
+  databaseURL: "https://beam-fdfaa-default-rtdb.firebaseio.com"
+});
+
+const databaseReference = firebaseAdmin.database().ref();
+databaseReference.once("value")
+  .then(function(snapshot) {
+    var name = snapshot.child("name").val();
+    console.log(name);
+});
 
 const app = express();
 app.use(express.static('public'));
@@ -20,9 +31,15 @@ app.use(
 );
 app.use(bodyParser.json());
 
-app.get('/', function (request, response, next) {
-    response.sendFile('./views/index.html', { root: __dirname })
+
+
+app.get('/', function (req, res, next) {
+    res.sendFile('./views/index.html', { root: __dirname })
 });
+
+app.get('/user/:userId', function(req, res) {
+    res.sendFile('./views/profile.html', { root: __dirname })
+})
 
 const server = app.listen (APP_PORT, function () {
     console.log('Beam server listening on port ' + APP_PORT);
