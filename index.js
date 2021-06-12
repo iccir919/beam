fdd33e729adb4fb73621050627e98c4efcd1d752
Neil Ricci <iccir919@gmail.com>
2021-06-12 11:51:32 -0700

    Add Plaid to server and Link to client

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
+require('dotenv').config()
 const express = require('express')
-const app = express()
 const admin = require('firebase-admin')
-
+const plaid = require('plaid')
+const app = express()
 const PORT = process.env.PORT || 8000
 
-admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
-  databaseURL: 'https://beam-94bf1-default-rtdb.firebaseio.com/'
-});
-
+app.use(express.urlencoded({extended: true}))
+app.use(express.json()) // To parse the incoming requests with JSON payloads
 app.use(express.static(__dirname + '/public'))
 
-// parse request bodies (req.body)
-app.use(express.urlencoded({ extended: true }))
+const plaidClient = new plaid.Client({
+  clientID: process.env.PLAID_CLIENT_ID,
+  secret: process.env.PLAID_SECRET,
+  env: plaid.environments.sandbox,
+  options: {
+    version: '2020-09-14',
+  },
+});
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://beam-94bf1-default-rtdb.firebaseio.com"
+});
 
 app.get('/', (req, res) => {
   res.sendFile('views/index.html', { root: __dirname })
@@ -20,6 +31,34 @@ app.get('/', (req, res) => {
 
 app.get('/profile', (req, res) => {
   res.sendFile('views/profile.html', { root: __dirname })
+})
+
+app.post('/api/token/get', (req, res) => {
+  const response = plaidClient
+  .createLinkToken({
+    user: {
+      client_user_id: '123-test-user-id',
+    },
+    client_name: 'Plaid Test App',
+    products: ['transactions'],
+    country_codes: ['US'],
+    language: 'en',
+    webhook: 'https://sample-web-hook.com',
+    account_filters: {
+      depository: {
+        account_subtypes: ['checking', 'savings'],
+      },
+    },
+  })
+  .then((response) => {
+    const linkToken = response.link_token;
+    res.json({ "linkToken": linkToken })
+  })
+  .catch((err) => {
+    // handle error
+  });
+
+
 })
 
 app.listen(PORT, () => {
