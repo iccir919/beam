840bb65ffd5750688970ffbd7543985385b7a067
Neil Ricci <iccir919@gmail.com>
2022-11-25 17:48:56 -0800

    Restart

--- config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    apiKey: "AIzaSyBMBn_hW0zq-o068OSiAvYjsJHtRhfLKs8",
-    authDomain: "beam-ae619.firebaseapp.com",
-    projectId: "beam-ae619",
-    storageBucket: "beam-ae619.appspot.com",
-    messagingSenderId: "696185717284",
-    appId: "1:696185717284:web:7af3c24d46260689d60eb1"
-};
