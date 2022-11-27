5dae834ebada6a6ddc6871a7c2211717dbfac43d
Neil Ricci <iccir919@gmail.com>
2022-11-26 17:49:42 -0800

    Create basic landing page

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app
-    .use(express.static(path.join(__dirname, 'public')))
-    .set('view engine', 'ejs')
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs')
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 app.get('/', (req, res) => {
     res.render('pages/index');
