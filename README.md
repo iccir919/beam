e7e7b72934229d6146915c7653ee756f833da0b8
Neil Ricci <iccir919@gmail.com>
2022-11-27 19:24:21 -0800

    Add pg library and database connection

---
+++ README.md
@@ -0,0 +1,7 @@
+# Beam
+
+Illuminate Your Finances
+
+## Helpful Links
+
+- [PostgreSQL Documentation](https://www.postgresql.org/docs/6.3/c0105.htm)
--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const path = require('path');
+const { Client } = require('pg');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL
+});
+
+client.connect();
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs')
