604b80a7327b8411653ac63917ad2239ecbe2659
Neil Ricci <iccir919@gmail.com>
2022-12-06 11:25:19 -0800

    requires SSL connections

--- queries.js
+++ queries.js
@@ -4,7 +4,10 @@ const { Pool } = require('pg');
 const connectionString = process.env.DATABASE_URL || `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 const pool = new Pool({
-    connectionString
+    connectionString,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 const getUsers = (request, response) => {
